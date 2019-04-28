4d77b6c320203f36c24ab7ffc86a4b3b0f880081
Anurag Ajwani <anurag.ajwani@gmail.com>
2019-04-28 17:33:59 +0100

    Processing frames from the back camera

--- SimpleLaneDetection/ViewController.swift
+++ SimpleLaneDetection/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    private var captureSession: AVCaptureSession = AVCaptureSession()
+    private let videoDataOutput = AVCaptureVideoDataOutput()
 
+    // MARK: - UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.addCameraInput()
+        self.getFrames()
+        self.captureSession.startRunning()
     }
+    
+    // MARkK: - AVCaptureVideoDataOutputSampleBufferDelegate
+    
+    func captureOutput(
+        _ output: AVCaptureOutput,
+        didOutput sampleBuffer: CMSampleBuffer,
+        from connection: AVCaptureConnection) {
+        // here we can process the frame
+        print("did receive frame")
+    }
+    
+    // MARK: - Private functions
 
-
+    private func addCameraInput() {
+        guard let device = AVCaptureDevice.DiscoverySession(
+            deviceTypes: [.builtInWideAngleCamera, .builtInDualCamera, .builtInTrueDepthCamera],
+            mediaType: .video,
+            position: .back).devices.first else {
+                fatalError("No back camera device found, please make sure to run SimpleLaneDetection in an iOS device and not a simulator")
+        }
+        let cameraInput = try! AVCaptureDeviceInput(device: device)
+        self.captureSession.addInput(cameraInput)
+    }
+    
+    private func getFrames() {
+        videoDataOutput.videoSettings = [(kCVPixelBufferPixelFormatTypeKey as NSString) : NSNumber(value: kCVPixelFormatType_32BGRA)] as [String : Any]
+        videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        videoDataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "camera.frame.processing.queue"))
+        self.captureSession.addOutput(videoDataOutput)
+        guard let connection = self.videoDataOutput.connection(with: AVMediaType.video),
+            connection.isVideoOrientationSupported else { return }
+        connection.videoOrientation = .portrait
+    }
 }
-
